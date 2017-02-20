d5de2bd78904e7eea888b06131e4cfb5840f1365
Breckin Lassen <breckinlassen@mail.weber.edu>
2017-02-20 23:04:57 +0000

    third commit

--- muons_hw5_task2.c
+++ muons_hw5_task2.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  muons_hw5_task2.c
  *       	Usage:  ./muons_hw5_task2.c
- *    Description: task 1 Kilometer conversion
+ *    Description: task 2 Kilometer conversion
  *
  *
  *        Version:  1.0
@@ -16,15 +16,37 @@
 #include <stdlib.h>		/* For exit */
 
 /* Function Prototypes */
+float MtoK(float m);
 
 /* Main Program */
 int main(void)
 {
+	float convM;
+	int inc, Startmile, Endmile;
+	printf("Enter start point: \n");
+	scanf("%d", &Startmile);
+	printf("Enter end point: \n");
+	scanf("%d", &Endmile);
+	printf("Enter increments: \n");
+	scanf("%d", &inc); 
+
+	printf("Miles = Kilometers\n");
+		for (int j = Startmile; j <= Endmile; j++)
+		{
+			Startmile =+ 1;
+			convM = MtoK(Startmile);
+			printf("%3.1d   %10.2f\n", Startmile, convM);
+		}
 
 	return 0;
 }
 
-
 /* Function Defenitions */
+float MtoK(float m)
+{
+	float k;
+	k = m * 1.61;
+	return k;
+}
 
 
